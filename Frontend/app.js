b6696d4fca945932efed811c9d0ec02ab3a1ace3
Antra Priyadarshini <antrapriyadarshini9@gmail.com>
2026-04-19 10:49:49 +0530

    Update app.js

--- Frontend/app.js
+++ Frontend/app.js
@@ -9,7 +9,7 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
     statusText.style.color = "blue";
 
     try {
-        const response = await fetch('http://10.30.193.236:5000/api/auth/login', {
+        const response = await fetch('https://r-d-cell.onrender.com/api/auth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
